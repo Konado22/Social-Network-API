6711ed7c3c7bce8045b645ea82d823c38d5dad29
unknown <jessedraper2@gmail.com>
2022-01-17 13:39:12 -0700

    more controller work and setup, work on routes and add model info

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,1 +1,8 @@
-const app = require('ex')
+const app = require('express').Router()
+const { Users, Thoughts, Reactions } = require('../models')
+//all reactions
+//add reaction
+//update reaction
+//create reaction
+//delete reaction
+// module.exports = 
