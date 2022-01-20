9e5e90344eb67909ad0648d76907212bd95482e8
unknown <jessedraper2@gmail.com>
2022-01-20 10:05:45 -0700

    fix user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,44 +10,53 @@ const allUsers = async (req, res) => {
 //get user by id
 const userById = async (req, res) => {
   try {
-    await Users.findOne({
-      where: {
-        _id: req.params.id,
-      },
-    });  
+    await Users.find({})
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((users) => {
+        const user = {
+          users
+        }
+        res.status(200).json(user);
+      });
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 };
 //update user
 const updateUser = async (req, res) => {
   try {
-    await Users.findOneAndUpdate({
-      where: {
-        _id: req.body.id,
-        username: req.body.username,
-        email: req.body.email,
-        //.need thoughts and friends?
-      },
-    });  
+    const updatedUser = await Users.findOneAndUpdate({ _id: req.params.id },
+    { $set: req.body },
+    { runValidators: true, new: true })
+    res.status(200).json(updatedUser)
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 };
 //create user
 const createUser = async (req, res) => {
-  await Users.create(req.body);
+  try {
+    await Users.create(req.body).then(() => {
+      res.json(req.body);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
+
 //delete user
 const deleteUser = async (req, res) => {
   try {
     await Users.findOneAndDelete({
       where: {
-          _id:req.params.id
+        _id: req.params.id,
       }
-  });
+    })
+    res.status(200).json('The user has been deleted')
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 };
-module.exports = allUsers , userById , updateUser, createUser, deleteUser
+(module.exports = allUsers), userById, updateUser, createUser, deleteUser;
